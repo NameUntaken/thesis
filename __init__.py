0d3c26ca36b77974ce93b64e84a6ab214359496d
suiran90 <122906593@qq.com>
2020-01-09 10:12:38 +0800

    first commit

---
+++ __init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+def covert_currency(a, b):
+                return a + b
+
+current_num = input("input a:")
+
+# 12CNY eval(12CNY[-3:]) -> 12
+out_num = covert_currency(eval(current_num), 2)
+
+print(out_num)
+"""
+
+
+def my_test():
+    # print("1" + str("1"))
+    """
+    file_name = "F:/python/requirements.txt"
+    fp = open(file_name, "r")
+    content = fp.read()
+    fp.close()
+    print(content)"""
+
+    # print("%(title)s" % {"title": "1"})
+    '''
+    dict1 = {'title': 'a', 'body': 'b'}
+    dict1['class'] = 3
+    dict1['title'] = 'ti' '''
+    print("hello world")
+
+
+def main():
+    """
+    a = lambda x: x * 2
+    print(a(3))
+    """
+    my_test()
+
+
+if __name__ == "__main__":
+    main()
